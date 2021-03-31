b54386500cc84f69fa851512ed3956a4e576f54c
pobrelkey <pobrelkey@users.noreply.github.com>
2021-03-31 23:25:41 +0100

    read channels from an ordinary m3u file

--- stream-remuxer.sh
+++ stream-remuxer.sh
@@ -28,7 +28,7 @@
 
 # User-tweakable parameters...
 LISTEN_PORT="${LISTEN_PORT:-9979}"
-CHANNELS_TXT="${CHANNELS_TXT:-$(dirname "${0}")/channels.txt}"
+CHANNELS_M3U="${CHANNELS_M3U:-$(dirname "${0}")/channels.m3u}"
 BASE_ADDR="${BASE_ADDR:-127.0.0.1}"
 
 
@@ -36,10 +36,10 @@ BASE_ADDR="${BASE_ADDR:-127.0.0.1}"
 # (no user-serviceable parts below this line)
 
 
-# Check that the channels.txt file exists
-if [[ ! -e "${CHANNELS_TXT}" ]]
+# Check that the channels.m3u file exists
+if [[ ! -e "${CHANNELS_M3U}" ]]
 then
-	echo "ERROR: must have a channels.txt file" 1>&2 
+	echo "ERROR: must have a channels.m3u file" 1>&2 
 	exit 1
 fi
 
@@ -47,19 +47,42 @@ fi
 # inetd-style, i.e. with standard input/output piped to the TCP connection.
 if [[ "${1}" != '--inetd' ]]
 then
-	echo "Listening on port ${LISTEN_PORT} - channels in ${CHANNELS_TXT}"
+	echo "Listening on port ${LISTEN_PORT} - channels in ${CHANNELS_M3U}"
 	exec busybox nc -ll -w 0 -p "${LISTEN_PORT}" -e "${0}" --inetd
 fi
 
 # Read in the channels.txt content into a Bash associative array
-declare -A CHANNELS
+declare -A CHANNEL_EXTINFS
+declare -A CHANNEL_URLS
+CHANNEL_EXTINF=
 while read LINE
 do
-	if [[ "${LINE}" =~ ^[^#].*\| ]]
+	if [[ "${LINE}" =~ ^#EXTINF: ]]
 	then
-		CHANNELS["${LINE%%|*}"]="${LINE#*|}"
+		CHANNEL_EXTINF="${LINE}"
+	elif [[ "${LINE}" =~ ^[^#] ]]
+	then
+		if [[ "${CHANNEL_EXTINF}" =~ \ sr-id=\".*\" ]]
+		then
+			CHANNEL_ID="${CHANNEL_EXTINF#* sr-id=\"}"
+			CHANNEL_ID="${CHANNEL_ID%%\"*}"
+		elif [[ "${CHANNEL_EXTINF}" =~ \ tvg-id=\".*\" ]]
+		then
+			CHANNEL_ID="${CHANNEL_EXTINF#* tvg-id=\"}"
+			CHANNEL_ID="${CHANNEL_ID%%\"*}"
+		elif [[ "${CHANNEL_EXTINF}" =~ , ]]
+		then
+			CHANNEL_ID="${CHANNEL_EXTINF##*,}"
+		else
+			# ugly but better than nothing...
+			CHANNEL_ID="${LINE}"
+		fi
+		CHANNEL_ID="${CHANNEL_ID//[^-_a-zA-Z0-9]/_}"
+		CHANNEL_EXTINFS["${CHANNEL_ID}"]="${CHANNEL_EXTINF}"
+		CHANNEL_URLS["${CHANNEL_ID}"]="${LINE}"
+		CHANNEL_EXTINF=
 	fi
-done < "${CHANNELS_TXT}"
+done < "${CHANNELS_M3U}"
 
 # read HTTP headers, ignore all but the URI
 read METHOD URI VERSION
@@ -104,19 +127,9 @@ case ${URI} in
 		echo -ne 'Connection: close\015\012'
 		echo -ne '\015\012'
 		echo '#EXTM3U'
-		for CHANNEL_ID in "${!CHANNELS[@]}"
+		for CHANNEL_ID in "${!CHANNEL_URLS[@]}"
 		do
-			EXTINF_TAGS="${CHANNELS["${CHANNEL_ID}"]%%|*}"
-			if [[ "x${EXTINF_TAGS}" == x ]]
-			then
-				EXTINF_TAGS="${CHANNEL_ID}"
-			fi
-			if [[ ! "${EXTINF_TAGS}" =~ , ]]
-			then
-				echo "#EXTINF:-1,${EXTINF_TAGS}"
-			else
-				echo "#EXTINF:-1 ${EXTINF_TAGS}"
-			fi
+			echo "${CHANNEL_EXTINFS["${CHANNEL_ID}"]}"
 			echo "http://${BASE_ADDR}:${LISTEN_PORT}/stream/${CHANNEL_ID}"
 		done
 		;;
@@ -136,9 +149,9 @@ case ${URI} in
 		<p><a href='/playlist.m3u'>Playlist</a></p>
 		<p>Streams:<ul>
 		__HEADER__
-		for CHANNEL_ID in "${!CHANNELS[@]}"
+		for CHANNEL_ID in "${!CHANNEL_URLS[@]}"
 		do
-			EXTINF_TAGS="${CHANNELS["${CHANNEL_ID}"]%%|*}"
+			EXTINF_TAGS="${CHANNEL_EXTINFS["${CHANNEL_ID}"]}"
 			if [[ "x${EXTINF_TAGS}" == x ]]
 			then
 				EXTINF_TAGS="${CHANNEL_ID}"
@@ -153,12 +166,12 @@ case ${URI} in
 	/stream/*)
 		URI2="${URI#/stream/}"
 		CHANNEL_ID="${URI2%%\?*}"
-		if [[ ! -v CHANNELS["${CHANNEL_ID}"] ]]
+		if [[ ! -v CHANNEL_URLS["${CHANNEL_ID}"] ]]
 		then
 			notfound
 			exit 0
 		fi
-		EXTINF_TAGS="${CHANNELS["${CHANNEL_ID}"]%%|*}"
+		EXTINF_TAGS="${CHANNEL_EXTINFS["${CHANNEL_ID}"]%%|*}"
 		if [[ "x${EXTINF_TAGS}" == x ]]
 		then
 			EXTINF_TAGS="${CHANNEL_ID}"
@@ -169,17 +182,16 @@ case ${URI} in
 		else
 			CHANNEL_NAME="${EXTINF_TAGS}"
 		fi
-		CHANNEL_URL_AND_TRANSCODE_OPTS="${CHANNELS["${CHANNEL_ID}"]#*|}"
-		if [[ "${CHANNEL_URL_AND_TRANSCODE_OPTS}" =~ \| ]]
+		if [[ "${EXTINF_TAGS}" =~ \ sr-transcode-opts=\".*\" ]]
 		then
-			CHANNEL_URL="${CHANNEL_URL_AND_TRANSCODE_OPTS%%|*}"
-			TRANSCODE_OPTS="${CHANNEL_URL_AND_TRANSCODE_OPTS#*|}:"
+			TRANSCODE_OPTS="${EXTINF_TAGS#* sr-transcode-opts=\"}"
+			TRANSCODE_OPTS="${TRANSCODE_OPTS%%\"*}:"
 		else
-			CHANNEL_URL="${CHANNEL_URL_AND_TRANSCODE_OPTS}"
 			TRANSCODE_OPTS=""
 		fi
 		echo -ne 'HTTP/1.0 200 OK\015\012'
 		echo -ne 'Content-type: video/MP2T\015\012'
+
 		echo -ne 'Pragma: no-cache\015\012'
 		echo -ne 'Connection: close\015\012'
 		echo -ne '\015\012'
@@ -188,14 +200,14 @@ case ${URI} in
 			cvlc -I dummy -V vdummy -A adummy --no-dbus \
 				--no-random --no-loop --no-repeat \
 				--telnet-password "${NONCE}" \
-				"${CHANNEL_URL}" \
+				"${CHANNEL_URLS["${CHANNEL_ID}"]}" \
 				--sout="#${TRANSCODE_OPTS}file{mux=ts,dst=/dev/fd/3}" \
 				</dev/null 1>&2
 		) 3>&1 | (
 			# copy output from VLC until we can write no more (i.e. client disconnects)...
 			cat 2>/dev/null
 			# ...then forcibly kill VLC as otherwise it'll stream forever
-			VLC_PID="$(ps auwwx | awk "(/cvlc -I dummy/ && /${NONCE}/ && !/awk/){print \$2}")"
+			VLC_PID="$(ps auwwx | awk "(/-I dummy -V dummy -A dummy/ && /${NONCE}/ && !/awk/){print \$2}")"
 			if [[ "x${VLC_PID}" != x ]]
 			then
 				kill -9 ${VLC_PID}
