5b0e97ac4d41705cfe36efc37765f37cfb8b6633
pobrelkey <pobrelkey@users.noreply.github.com>
2021-04-03 12:39:49 +0100

    more comments; break up long lines for legibility; don't use an external binary to generate TS IDs

--- stream-remuxer.sh
+++ stream-remuxer.sh
@@ -65,8 +65,14 @@ done
 if [[ -t 1 ]]
 then
 	echo "Listening on port ${SR_LOCAL_PORT} - channels in ${SR_CHANNELS_M3U}"
-	exec busybox nc -ll -w 0 -p "${SR_LOCAL_PORT}" "${SR_LOCAL_ADDR}" -e "${0}" -a "${SR_LOCAL_ADDR}" -p "${SR_LOCAL_PORT}" -c "${SR_CHANNELS_M3U}"
+	exec busybox nc -ll -w 0 -p "${SR_LOCAL_PORT}" "${SR_LOCAL_ADDR}" \
+		-e "${0}" \
+			-a "${SR_LOCAL_ADDR}" \
+			-p "${SR_LOCAL_PORT}" \
+			-c "${SR_CHANNELS_M3U}"
 fi
+
+FOOTER="<hr /><p><a href='/'>Stream Remuxer</a> at ${SR_LOCAL_ADDR} port ${SR_LOCAL_PORT}</p>"
 
 # Check that the channels.m3u file exists
 if [[ ! -e "${SR_CHANNELS_M3U}" ]]
@@ -76,19 +82,28 @@ then
 	echo -ne "Pragma: no-cache\015\012"
 	echo -ne "Connection: close\015\012"
 	echo -ne "\015\012"
-	echo "<html><head><title>Configuration Error</title></head><body><h1>Configuration Error</h1><p>Must have a <tt>channels.m3u</tt> file</p>${FOOTER}</body></html>"
+	cat <<-__500__
+		<html><head>
+		<title>Configuration Error</title>
+		</head><body>
+		<h1>Configuration Error</h1>
+		<p>Must have a <tt>channels.m3u</tt> file</p>
+		${FOOTER}
+		</body></html>
+	__500__
 	exit 1
 fi
 
-FOOTER="<hr /><p><a href='/'>Stream Remuxer</a> at ${SR_LOCAL_ADDR} port ${SR_LOCAL_PORT}</p>"
-
-# Read in the channels.m3u content into a Bash associative array
+# Read in the channels.m3u content into two Bash associative arrays
 declare -A CHANNEL_EXTINFS
 declare -A CHANNEL_URLS
 CHANNEL_ID=
 CHANNEL_EXTINF=
+LINE=
 while true
 do
+	# read in the line (we don't do "while read LINE" in case
+	# the last line in the M3U doesn't end with a newline)
 	read LINE
 	READSTATUS=$?
 	if [[ "${LINE}" =~ ^.?.?.?#EXTM3U ]]
@@ -97,9 +112,12 @@ do
 		true
 	elif [[ "${LINE}" =~ ^#EXTINF: ]]
 	then
+		# an EXTINF line - save this, don't parse it until the URL appears
 		CHANNEL_EXTINF="${LINE}"
 	elif [[ "${LINE}" =~ ^[^#] ]]
 	then
+		# a non-blank line - presumably a channel URL
+		# now try to determine the channel ID from tags in the EXTINF
 		if [[ "${CHANNEL_EXTINF}" =~ \ sr-id=\".*\" ]]
 		then
 			CHANNEL_ID="${CHANNEL_EXTINF#* sr-id=\"}"
@@ -110,25 +128,41 @@ do
 			CHANNEL_ID="${CHANNEL_EXTINF#* tvg-id=\"}"
 			CHANNEL_ID="${CHANNEL_ID%%\"*}"
 		fi
+		# if no channel ID tags in the EXTINF, use the channel's name
 		if [[ "x${CHANNEL_ID}" == x && "${CHANNEL_EXTINF}" =~ , ]]
 		then
 			CHANNEL_ID="${CHANNEL_EXTINF##*,}"
 		fi
+		# failing the above, just use the channel's URL
 		if [[ "x${CHANNEL_ID}" == x ]]
 		then
-			# ugly but better than nothing...
 			CHANNEL_ID="${LINE}"
 		fi
+		# ensure the channel ID contains no spaces/dodgy characters
 		CHANNEL_ID="${CHANNEL_ID//[^-_a-zA-Z0-9]/_}"
+		# if the playlist already contains a channel with this ID, 
+		# append a number to get a unique ID
+		if [[ -v CHANNEL_URLS["${CHANNEL_ID}"] ]]
+		then
+			N=1
+			while [[ -v CHANNEL_URLS["${CHANNEL_ID}_${N}"] ]]
+			do
+				N="$(( ${N} + 1 ))"
+			done
+			CHANNEL_ID="${CHANNEL_ID}_${N}"
+		fi
+		# save channel info to the associative arrays (and reset state)
 		CHANNEL_EXTINFS["${CHANNEL_ID}"]="${CHANNEL_EXTINF}"
 		CHANNEL_URLS["${CHANNEL_ID}"]="${LINE}"
-    CHANNEL_ID=
+		CHANNEL_ID=
 		CHANNEL_EXTINF=
 	fi
+	# if the line we just read was the last one, exit the loop
 	if [[ ${READSTATUS} != 0 ]]
 	then
 		break
 	fi
+	LINE=
 done < "${SR_CHANNELS_M3U}"
 
 # read HTTP headers, ignore all but the URI
@@ -145,10 +179,18 @@ then
 	echo -ne "Pragma: no-cache\015\012"
 	echo -ne "Connection: close\015\012"
 	echo -ne "\015\012"
-	echo "<html><head><title>Unsupported method</title></head><body><h1>Unsupported method</h1></body>${FOOTER}</html>"
+	cat <<-__501__
+		<html><head>
+		<title>Unsupported Method</title>
+		</head><body>
+		<h1>501 Unsupported Method</h1>
+		${FOOTER}
+		</body></html>
+	__501__
 	exit 0
 fi
 
+# write a simple "404 not found" response (used in a couple of places)
 function notfound() {
 	echo -ne 'HTTP/1.0 404 Not Found\015\012'
 	echo -ne 'Content-type: text/html\015\012'
@@ -156,13 +198,13 @@ function notfound() {
 	echo -ne 'Connection: close\015\012'
 	echo -ne '\015\012'
 	cat <<-__404__
-	<html><head>
-	<title>Not Found</title>
-	</head><body>
-	<h1>404 Not Found</h1>
-	${FOOTER}
-	</body></html>
-__404__
+		<html><head>
+		<title>Not Found</title>
+		</head><body>
+		<h1>404 Not Found</h1>
+		${FOOTER}
+		</body></html>
+	__404__
 }
 
 case ${URI} in
@@ -189,12 +231,12 @@ case ${URI} in
 		echo -ne 'Connection: close\015\012'
 		echo -ne '\015\012'
 		cat <<-__HEADER__
-		<html><head>
-		<title>stream remuxer (${SR_LOCAL_ADDR}:${SR_LOCAL_PORT})</title>
-		</head><body>
-		<h1>Stream Remuxer</h1>
-		<p><a href='/playlist.m3u'>Playlist</a></p>
-		<p>Streams:<ul>
+			<html><head>
+			<title>stream remuxer (${SR_LOCAL_ADDR}:${SR_LOCAL_PORT})</title>
+			</head><body>
+			<h1>Stream Remuxer</h1>
+			<p><a href='/playlist.m3u'>Playlist</a></p>
+			<p>Streams:<ul>
 		__HEADER__
 		for CHANNEL_ID in "${!CHANNEL_URLS[@]}"
 		do
@@ -205,12 +247,12 @@ case ${URI} in
 			fi
 			echo "<li><a href='/stream/${CHANNEL_ID}'>${EXTINF_TAGS##*,}</a></li>"
 		done
-		echo '</ul></p>'
-		echo "${FOOTER}"
-		echo '</body></html>'
+		echo "</ul></p>${FOOTER}</body></html>"
 		;;
-	
+
 	/stream/*)
+		# actually serve up the MPEG-TS stream for a channel
+		# first... does a channel with the given ID exist?
 		CHANNEL_ID="${URI#/stream/}"
 		CHANNEL_ID="${CHANNEL_ID%%\?*}"
 		if [[ ! -v CHANNEL_URLS["${CHANNEL_ID}"] ]]
@@ -218,17 +260,19 @@ case ${URI} in
 			notfound
 			exit 0
 		fi
-		EXTINF_TAGS="${CHANNEL_EXTINFS["${CHANNEL_ID}"]%%|*}"
-		if [[ "x${EXTINF_TAGS}" == x ]]
-		then
-			EXTINF_TAGS="${CHANNEL_ID}"
-		fi
+		# parse channel name from the EXTINF - anything after the last comma
+		EXTINF_TAGS="${CHANNEL_EXTINFS["${CHANNEL_ID}"]}"
+		CHANNEL_NAME=
 		if [[ ! "${EXTINF_TAGS}" =~ , ]]
 		then
 			CHANNEL_NAME="${EXTINF_TAGS##*,}"
-		else
-			CHANNEL_NAME="${EXTINF_TAGS}"
 		fi
+		# ...falling back to the channel URL if that's blank/nonexistent
+		if [[ "x${CHANNEL_NAME}" == 'x' ]]
+		then
+			CHANNEL_NAME="${CHANNEL_URLS["${CHANNEL_ID}"]}"
+		fi
+		# parse transcode options, if any, from the EXTINF
 		if [[ "${EXTINF_TAGS}" =~ \ sr-transcode-opts=\".*\" ]]
 		then
 			TRANSCODE_OPTS="${EXTINF_TAGS#* sr-transcode-opts=\"}"
@@ -236,44 +280,53 @@ case ${URI} in
 		else
 			TRANSCODE_OPTS=""
 		fi
+		# use consistent values for network ID/stream ID in the generated TS,
+		# else TVHeadend complains - we can optionally set these in the EXTINF
 		if [[ "${EXTINF_TAGS}" =~ \ sr-net-id=\".*\" ]]
 		then
 			NET_ID="${EXTINF_TAGS#* sr-net-id=\"}"
 			NET_ID="${NET_ID%%\"*}:"
 		else
+			# arbitrary value in the private range (0xFF01-0xFFFF)  
 			NET_ID=65310
 		fi
 		if [[ "${EXTINF_TAGS}" =~ \ sr-ts-id=\".*\" ]]
 		then
 			TS_ID="${EXTINF_TAGS#* sr-ts-id=\"}"
 			TS_ID="${TS_ID%%\"*}:"
-		elif [[ "x$(which cksum)" != 'x' ]]
-		then
-			# use cksum to generate an integer TS ID from the channel ID
-			TS_ID="$(echo "${CHANNEL_ID}" | cksum)"
-			TS_ID="${TS_ID%% *}"
-			TS_ID="$((${TS_ID} % 65529))"
 		else
+			# assign a unique TS ID for each channel
 			TS_ID=1
+			for X in "${!CHANNEL_URLS[@]}"
+			do
+				if [[ "${X}" == "${CHANNEL_ID}" ]]
+				then
+					break
+				fi
+				TS_ID="$(( ${TS_ID} + 1 ))"
+			done
 		fi
+		# write HTTP headers in preparation for actually serving up content
 		echo -ne 'HTTP/1.0 200 OK\015\012'
 		echo -ne 'Content-type: video/MP2T\015\012'
 		echo -ne 'Pragma: no-cache\015\012'
 		echo -ne 'Connection: close\015\012'
 		echo -ne '\015\012'
+		# set up an exit trap which will kill VLC and clean up our FIFO
+		# after the client closes the connection
 		VLC_PID=
 		VLC_FIFO="/tmp/stream-remuxer.$$"
 		function onexit() {
 			if [[ "x${VLC_PID}" != 'x' ]]
 			then
-				# forcibly kill VLC as otherwise it plays the stream forever
 				kill -9 "${VLC_PID}"
 			fi
 			rm -f "${VLC_FIFO}"
 		}
 		trap onexit EXIT
+		# create a FIFO to which VLC will write its output
 		mkfifo -m 0600 "${VLC_FIFO}"
-		# start VLC in the background, writing to a FIFO...
+		# start VLC in the background, writing to the FIFO...
 		cvlc -I dummy -V vdummy -A adummy --no-dbus \
 			--no-random --no-loop --no-repeat \
 			--sout-ts-netid="${NET_ID}" --sout-ts-tsid="${TS_ID}" \
@@ -281,14 +334,15 @@ case ${URI} in
 			--sout="#${TRANSCODE_OPTS}file{mux=ts,dst=${VLC_FIFO}}" \
 			</dev/null 1>&2 &
 		VLC_PID=$!
-		# ...then have cat read from that fifo until it can write no more
-		# (i.e. the client disconnects)...
+		# ...then have cat read from that fifo until it can write no more 
+		# (i.e. the client closes the connection)...
 		cat "${VLC_FIFO}"
 		# ...at which the exit trap kicks in and kills VLC (complicated, but
 		# if we just ran VLC dumping to stdout, it wouldn't know when to quit)
 		;;
-	
+
 	*)
+		# request to unknown URL - so return a 404 error
 		notfound
 		;;
 
